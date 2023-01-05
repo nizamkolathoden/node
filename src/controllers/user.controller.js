ea813a8a5e434e498550fe388f3a3592222805fb
Nizam kolathoden <nizamkolathoden@Nizams-MacBook-Air.local>
2023-01-05 17:04:11 +0530

    small

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,5 +1,4 @@
 const User = require("../schema/user.schema");
-const Post = require("../schema/post.schema");
 module.exports.getUsersWithPostCount = async (req, res) => {
   try {
     //TODO: Implement this API
